3da163a93eb64bbc58e13039b6c1c3b0dd5d4194
Aasritha Sai Devireddy <aasrithasaidevireddy944@gmail.com>
2026-06-08 20:19:01 +0530

    Convert project to Vite React and fix Tailwind

---
+++ src/App.tsx
@@ -0,0 +1,32 @@
+import { Toaster } from "sonner";
+
+import { Navbar } from "./components/Navbar";
+import { About } from "./components/sections/About";
+import { Contact } from "./components/sections/Contact";
+import { EnquiryForm } from "./components/sections/EnquiryForm";
+import { Footer } from "./components/sections/Footer";
+import { Hero } from "./components/sections/Hero";
+import { Industries } from "./components/sections/Industries";
+import { Services } from "./components/sections/Services";
+import { WhyChooseUs } from "./components/sections/WhyChooseUs";
+import { Workflow } from "./components/sections/Workflow";
+
+export default function App() {
+  return (
+    <div className="min-h-screen bg-background text-foreground">
+      <Navbar />
+      <main>
+        <Hero />
+        <About />
+        <Services />
+        <Industries />
+        <WhyChooseUs />
+        <Workflow />
+        <EnquiryForm />
+        <Contact />
+      </main>
+      <Footer />
+      <Toaster position="top-right" richColors />
+    </div>
+  );
+}
