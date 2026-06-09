28399175098039274710a3d3ea4af99b40654fd2
Aasritha Sai Devireddy <aasrithasaidevireddy944@gmail.com>
2026-06-09 16:00:16 +0530

    Connect frontend to render backend

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,13 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST"],
+    allowedHeaders: ["Content-Type"],
+  })
+);
 app.use(express.json());
 app.get("/test", (req, res) => {
   res.json({
